2452af83046888c5f43b0ea3f6ba1aaaf6d6c670
anirban0184 <geekay0184@gmail.com>
2024-12-29 14:58:35 +0600

    quiz app done

--- quiz_app.php
+++ quiz_app.php
@@ -1,1 +1,37 @@
+<?php
  #Quiz-App 
+ $questions = [
+
+    ['question' => 'What is 2 + 2?', 'correct' => '4'],
+
+    ['question' => 'What is the capital of France?', 'correct' => 'Paris'],
+
+    ['question' => 'Who wrote "Hamlet"?', 'correct' => 'Shakespeare'],
+
+];
+$answers = [];
+foreach($questions as $index => $value) {
+    echo ($index + 1) . "." . $value['question'] . "\n";
+    $answers[] = trim(readline("Write Your Answer: "));
+}
+
+function evaluateQuiz(array $questions, array $answers) {
+    $score = 0;
+    foreach($questions as $index => $question) {
+        if($answers[$index] === $question["correct"]) {
+            $score++;
+        }
+    }
+    return $score;
+}
+$score = evaluateQuiz($questions, $answers);
+
+echo "Your Scored $score Out Of " . count($questions) . "\n";
+
+if ($score === count($questions)) {
+    echo "Excellent Job! \n";
+}elseif($score > 1) {
+    echo "Good Effort! \n";
+}else {
+    echo "Batter luck Next time";
+}
